abc9d2671982fb870a3a60b3ce017f5924057157
Your Name <you@example.com>
2024-01-20 01:17:02 -0500

    adding content

---
+++ module.py
@@ -0,0 +1,14 @@
+def SFC():
+
+    length=input("enter length in yards")
+    width=input("enter width in feet")
+    print(int(length)*int(width))
+
+
+def printName(name):
+    return f"Hello Mr/Ms {name}...we've been waiting for you!"
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,14 @@
+def circFinder():
+    
+    diameter=input("what is the diameter of the circle?")
+    print(3.14 * int(diameter))
+
+
+
+
+def squareftCalc():
+
+    length=input("enter length in feet")
+    width=input("enter width in feet")
+    print(int(length)*int(width))
+
